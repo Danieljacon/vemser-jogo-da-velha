db3dc0bbb028c31d1863be7fa93b6945bafbd6a0
Danieljacon <danielcamillo2020@gmail.com>
2022-10-18 16:39:56 -0300

    Funcionando mas com Jquery

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function casasIguais(a, b, c){
     var bgB = $("#casa"+b).css("background-image");
     var bgC = $("#casa"+c).css("background-image");
     if( (bgA == bgB) && (bgB == bgC) && (bgA != "none" && bgA != "")){
-        if(bgA.indexOf("1.png") >= 0)
+        if(bgA.indexOf("1.jpg") >= 0)
             vencedor = "1";
         else
             vencedor = "2";
@@ -20,5 +20,24 @@ function casasIguais(a, b, c){
     }
 }
 
+function verificarFimDeJogo(){
+    if( casasIguais(1, 2, 3) || casasIguais(4, 5, 6) || casasIguais(7, 8, 9) ||
+        casasIguais(1, 4, 7) || casasIguais(2, 5, 8) || casasIguais(3, 6, 9) ||
+        casasIguais(1, 5, 9) || casasIguais(3, 5, 7)
+        ){
+        $("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
+        $(".casa").off("click");
+    }
+}
 
 
+$(".casa").click(function(){
+    var bg = $(this).css("background-image");
+    if(bg == "none" || bg == "")
+    {
+        var fig = "url(" + vez.toString() + ".jpg)";
+        $(this).css("background", fig);
+        vez = (vez == 1 ? 2 : 1);
+        verificarFimDeJogo();
+    }
+});
