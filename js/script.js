523a6010a830363092d335ed0179b28645a14d16
Danieljacon <danielcamillo2020@gmail.com>
2022-10-18 16:42:20 -0300

    Funcionando mas com Jquery

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ var vez = 1;
 var vencedor = "";
 
 function casasIguais(a, b, c){
-    var casaA = $("#casa"+a);
-    var casaB = $("#casa"+b);
-    var casaC = $("#casa"+c);
     var bgA = $("#casa"+a).css("background-image");
     var bgB = $("#casa"+b).css("background-image");
     var bgC = $("#casa"+c).css("background-image");
